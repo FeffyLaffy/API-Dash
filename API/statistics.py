ea71712bbe52e3b097e1cac0581884f4695e1a22
Tiffceet <zkang.loo@gmail.com>
2023-02-14 19:04:53 +0800

    feat: statistics base code

---
+++ API/statistics.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify, request, Response, url_for, redirect, session
+
+statistics = Blueprint('statistics', __name__)
+
+
+@statistics.route("/statistics/pc-play-statistics-feedback", methods=['POST'])
+def pcPlayStatsFeedback():
+    return
